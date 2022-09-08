77924da2a08191a7ea738e4cfd14b17025c1ee46
Anti Coding Club <73062879+Thoybur-Rohman@users.noreply.github.com>
2022-09-08 15:22:11 +0100

    Update

--- src/components/InfoCards.jsx
+++ src/components/InfoCards.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "./commonStructure/Card";
-import aim from "../css/images/Target_Outline.png"
+import aim from "../css/images/Motivation_Outline.png"
 import teams from "../css/images/Team presentation _Outline.png"
 import student from "../css/images/Team work_Two Color.png"
 import "../css/infoCards.css";
@@ -14,18 +14,19 @@ export default function InfoCards() {
         // text="Our staff base is uniquely identifiable by our student talent and leading experts."
         text="Our talented students deliver projects guided by leading experts."
       />
+      <Card
+        image={student}
+        title="Community Impact"
+        text="Our IT and R&D services are designed to advance SMEs, Start-ups and Large businesses in West London."
+      />
 
       <Card
         image={aim}
         title="Competitive Prices"
-        text="We are a non-profit organisation providing quality services for a fraction of the industry price."
+        text="We provide quality services without the industry’s profit margins"
       />
 
-      <Card
-        image={student}
-        title="Community Impact"
-        text="We beat our competition with viable prices, whilst matching their quality of work."
-      />
+
 
     </div>
   );
--- src/components/PageIntro.jsx
+++ src/components/PageIntro.jsx
@@ -22,7 +22,7 @@ export default function PageIntro() {
         <div className="text-container">
           <h1 className="intro-title">Brunel <span className="talent">Talent</span> Market Place</h1>
           <p className="intro-text">
-            A non-profit IT organisation aiming to foster symbiotic relations between industry and academia.
+            We are an IT organisation inside Brunel University that aims to foster symbiotic relations between industry and academia.
           </p>
           <div className="btn-row">
             <button className="video btn" onClick={() => setToggle(!toggle)}>
