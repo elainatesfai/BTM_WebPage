f3359118f0b5ed0f211805c53a812dbc61e20e1d
elainatesfai <elaina.tesfai@gmail.com>
2022-09-14 00:01:16 +0100

    Small Iterations
    - changed some descriptions in the team's js file
    - changed info cards Title CSS
    - changed the colour of the button when hovering
    - Copied Marwan's code for Talent and Expert Card

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -14,40 +14,14 @@ import Samuel from "../images/Talents/SamuelAsareBotwe.png"
 import Ritu from "../images/Talents/RituGulia.png"
  
 const items = [
-  // <TalentCard
-  //     image="https://purepng.com/public/uploads/large/businessman-onj.png"
-  //     name="Phillip Bibik"
-  //     work="Programmer"
-  //     description="Eveniet quaerat aspernatur illo natus modi vitae nam commodi obcaecati nostrum! Molestias officiis adipisci aliquid"
-  //     data-value="1"
-  //   />,
- 
-  <TalentCard
-    image={Islam}
-    name="Islam Raihan"
-    work="Front-end Developer"
-    description="I am an enthusiastic individual who is fascinated by technology and its constant advancements.
-          I am currently a junior developer at Glue Reply."
-    linkedIn="https://www.linkedin.com/in/islam-raihan-714784195/"
-    data-value="1"
- 
-  />,
- 
-  // <TalentCard
-  //   image="https://purepng.com/public/uploads/large/businessman-onj.png"
-  //   name="Mandeep Singh"
-  //   work="Programmer"
-  //   description="Eveniet quaerat aspernatur illo natus modi vitae nam commodi obcaecati nostrum! Molestias officiis adipisci aliquid"
-  //   data-value="3"
-  // />,
  
   <TalentCard
     image={Thoybur}
     name="Thoybur Rohman"
-    work="Programmer/Executive Team"
+    work="Full-stack Developer/Executive Team"
     description="I am strong-willed and strive to make my initial plans and prototypes a success regardless of the obstacles that stand in the way. This will facilitate me in becoming successful in this career."
     linkedIn="https://www.linkedin.com/in/thoybur/"
-    data-value="2"
+    data-value="1"
   />,
  
   <TalentCard
@@ -56,7 +30,7 @@ const items = [
     work="Front-end Developer/Executive Team"
     description="I am a dependable, quick-thinking and responsible individual that thoroughly enjoys challenging myself and pushing the boundaries of what I can achieve."
     linkedIn="https://www.linkedin.com/in/elaina-tesfai-213a19243/"
-    data-value="3"
+    data-value="2"
   />,
  
   <TalentCard
@@ -65,7 +39,18 @@ const items = [
     work="Front-end Developer/Executive Team"
     description="I am a passionate, quick learning programmer that constantly broaden my knowledge in different languages to earn more experience in this field."
     linkedIn="https://www.linkedin.com/in/marwan-haj-mohamed-305578218/"
+    data-value="3"
+  />,
+
+  <TalentCard
+    image={Islam}
+    name="Islam Raihan"
+    work="Front-end Developer"
+    description="I am an enthusiastic individual who is fascinated by technology and its constant advancements.
+          I am currently a junior developer at Glue Reply."
+    linkedIn="https://www.linkedin.com/in/islam-raihan-714784195/"
     data-value="4"
+ 
   />,
  
   <TalentCard
@@ -82,7 +67,7 @@ const items = [
     name=" Samuel Asare Botwe"
     work="Data Analyst"
     description="I am a very motivated data analyst and computer programmer that is willing to learn and use their skills to provide problem-solving strategies and applications."
-    linkedIn="https://www.linkedin.com/in/shrawanimenghal/"
+    linkedIn="https://www.linkedin.com/in/samuel-botwe-b130491b9/?originalSubdomain=gh"
     data-value="6"
   />,
 
@@ -90,9 +75,9 @@ const items = [
     image={Ritu}
     name="Ritu Gulia"
     work="Data Analyst"
-    description="“I enjoy problem-solving and am passionate about learning and diving deeper into data. I am highly imaginative and eager to learn, which helps me make challenging judgements. "
-    linkedIn="https://www.linkedin.com/in/shrawanimenghal/"
-    data-value="7"
+    description="I enjoy problem-solving and am passionate about learning and diving deeper into data. I am highly imaginative and eager to learn, which helps me make challenging judgements. "
+    linkedIn="https://www.linkedin.com/in/ritu-gulia-18b73b175/?originalSubdomain=in"
+    data-value="8"
   />
 ]
  
