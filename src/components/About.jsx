8928e0dae53affc9588695694e1f7786a071e31a
elainatesfai <elaina.tesfai@gmail.com>
2022-12-01 02:25:45 +0000

    Some more of Alan's small iterations
    Specified that we are an IT consulting organisation, added the new and improved company culture statement. Added a missing talent (Avi Sharma). Finally began the for the news section -> temporarily commented it out for now. Will work on it further later on.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,30 +1,68 @@
 import React from "react";
+
+import SlideCard from "./commonStructure/SlideCard";
 import "../css/about.css";
 import InfoCards from "./InfoCards";
 import "../css/infoCards.css";
- 
+
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+
+import image1 from '../images/slide1-transformed.png'
+import image2 from '../images/slide2.png'
+
+const items = [
+
+  <SlideCard
+    image={image1}
+    title='Percentage of highly cited paper in Computer Science Artifical intelligence'
+    description='our university is acknowledged'
+  />, 
+  <SlideCard 
+    image={image2}
+    title="Computing professor one of the world's most highly cited researchers – nine years in a row"
+    description='...'
+  />
+]
+
 export default function App() {
   return (
     <div className="about" id="about">
       <div className="about-title">Why Choose Us</div>
       <InfoCards />
+
         <div className="about-container">
-        <div className="about-image-container">
-          <div className="about-image"></div>
-        </div>
-        <div className="about-text-container">
-          <h2> Our vision </h2>
-          <ul className="list">
-            <li>To become a recognisable and viable IT consultancy known for its Research & Development services for West London businesses.</li>
-            <li>
-              We aspire to give 2% of the computer department’s student cohort the opportunity to participate in BTM projects.
-            </li>
-            <li>
-              We aim to expand this model to other university departments, such as design, business management, and mechanical engineering
-            </li>
-          </ul>
+          <div className="about-image-container">
+            <div className="about-image" />
+          </div>
+          <div className="about-text-container">
+            <h2> Our vision </h2>
+            <ul className="list">
+              <li>To become a recognisable and viable IT consultancy known for its Research & Development services for West London businesses.</li>
+              <li>
+                We aspire to give 2% of the computer department’s student cohort the opportunity to be employed in BTM projects.
+              </li>
+              <li>
+                We aim to expand this model to other university departments, such as design, business management, and mechanical engineering
+              </li>
+            </ul>
         </div>
       </div>
-    </div>  
+
+    {/* <div className='about-container'>
+      <AliceCarousel
+          mouseTracking
+          items={items}
+          // responsive={responsive}
+          autoPlay
+          autoPlayDirection="ltr"
+          infinite
+          autoPlayInterval={5600}
+          animationDuration={1300}
+        />
+    </div> */}
+
+    </div>
+ 
   );
 }
